395d4c298ae06da3456a4fd1f69d01a7be38f5a5
Reto Ryter <reto@twy.gmbh>
2021-07-22 18:00:52 +0000

    chore(typedoc): use config file to achieve propper output

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-export * from './lib/lsp-factory';
+export { LSPFactory } from './lib/lsp-factory';
+export { LSP3UniversalProfile } from './lib/classes/lsp3-universal-profile';
+export * from './lib/interfaces';
