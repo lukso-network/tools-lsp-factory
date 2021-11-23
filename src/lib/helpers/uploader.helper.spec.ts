76c4d319278836e61ee9f1b5ab99009825aea0c1
Callum <callum@lukso.io>
2021-11-23 17:53:08 +0100

    test: default jest to use fake timers

--- src/lib/helpers/uploader.helper.spec.ts
+++ src/lib/helpers/uploader.helper.spec.ts
@@ -17,89 +17,95 @@ jest.mock('ipfs-http-client');
 jest.mock('browser-image-compression');
 
 const file = new File(['FileContents'], 'file-name');
-describe('#ipfsUpload', () => {
-  it('should pin images', async () => {
-    const addMock = jest.fn();
-    (create as jest.Mock).mockReturnValue({
-      add: addMock,
+describe('uploader.helper.spec.ts', () => {
+  beforeEach(() => {
+    jest.useRealTimers();
+  });
+
+  describe('#ipfsUpload', () => {
+    it('should pin images', async () => {
+      const addMock = jest.fn();
+      (create as jest.Mock).mockReturnValue({
+        add: addMock,
+      });
+
+      const upload = await ipfsUpload(file, {});
+
+      expect(addMock).toHaveBeenCalledWith(file, { pin: true });
+      expect(upload).toEqual(undefined);
+    });
+  });
+
+  describe('#imageUpload', () => {
+    it('should throw an error on invalid input', async () => {
+      await expect(
+        imageUpload(
+          new File(['sdfasdf'], 'file-name', {
+            type: 'zip',
+          }),
+          {
+            ipfsClientOptions: {},
+          }
+        )
+      ).rejects.toThrowError("File type: 'zip' does not start with 'image/'");
     });
 
-    const upload = await ipfsUpload(file, {});
+    it('should pin files when using IPFS', async () => {
+      const { file, addMock } = await mockDependencies();
+      await imageUpload(file, { ipfsClientOptions: {} });
 
-    expect(addMock).toHaveBeenCalledWith(file, { pin: true });
-    expect(upload).toEqual(undefined);
-  });
-});
-
-describe('#imageUpload', () => {
-  it('should throw an error on invalid input', async () => {
-    await expect(
-      imageUpload(
-        new File(['sdfasdf'], 'file-name', {
-          type: 'zip',
-        }),
-        {
-          ipfsClientOptions: {},
-        }
-      )
-    ).rejects.toThrowError("File type: 'zip' does not start with 'image/'");
-  });
-
-  it('should pin files when using IPFS', async () => {
-    const { file, addMock } = await mockDependencies();
-    await imageUpload(file, { ipfsClientOptions: {} });
-
-    expect(addMock).toHaveBeenCalledWith(file, { pin: true });
-  });
-
-  it('should resize images', async () => {
-    const { file, drawFileInCanvasSpy } = await mockDependencies();
-    await imageUpload(file, { ipfsClientOptions: {} });
-
-    expect(imageCompression).toHaveBeenCalledTimes(5);
-    expect(drawFileInCanvasSpy).toBeCalledTimes(5);
-  });
-
-  it('should accept images as buffer', async () => {
-    const { addMock } = await mockDependencies();
-
-    const imageResponse = await axios.get('https://picsum.photos/200/300', {
-      responseType: 'arraybuffer',
+      expect(addMock).toHaveBeenCalledWith(file, { pin: true });
     });
-    const buffer = Buffer.from(imageResponse.data as string, 'binary');
 
-    const result = await imageUpload(
-      { buffer, mimeType: SupportedImageBufferFormats.MIME_JPEG },
-      { ipfsClientOptions: {} }
-    );
+    it('should resize images', async () => {
+      const { file, drawFileInCanvasSpy } = await mockDependencies();
+      await imageUpload(file, { ipfsClientOptions: {} });
 
-    expect(addMock).toHaveBeenCalled();
-
-    expect(result.length === 5);
-    expect(result[0]).toHaveProperty('width');
-    expect(result[0]).toHaveProperty('height');
-    expect(result[0]).toHaveProperty('hashFunction');
-    expect(result[0]).toHaveProperty('hash');
-    expect(result[0]).toHaveProperty('url');
-  });
-});
-
-describe('#resizeBuffer', () => {
-  it('should resize buffer images', async () => {
-    const imageResponse = await axios.get('https://picsum.photos/200/300', {
-      responseType: 'arraybuffer',
+      expect(imageCompression).toHaveBeenCalledTimes(5);
+      expect(drawFileInCanvasSpy).toBeCalledTimes(5);
     });
-    const buffer = Buffer.from(imageResponse.data as string, 'binary');
 
-    const size = 10;
-    const result = await resizeBuffer(buffer as Buffer, 'image/jpeg', size);
+    it('should accept images as buffer', async () => {
+      const { addMock } = await mockDependencies();
 
-    expect(result.byteLength < buffer.byteLength);
+      const imageResponse = await axios.get('https://picsum.photos/200/300', {
+        responseType: 'arraybuffer',
+      });
+      const buffer = Buffer.from(imageResponse.data as string, 'binary');
 
-    const resizedDimensions = imageSize(result);
+      const result = await imageUpload(
+        { buffer, mimeType: SupportedImageBufferFormats.MIME_JPEG },
+        { ipfsClientOptions: {} }
+      );
 
-    expect(resizedDimensions.height <= size);
-    expect(resizedDimensions.width <= size);
+      expect(addMock).toHaveBeenCalled();
+
+      expect(result.length === 5);
+      expect(result[0]).toHaveProperty('width');
+      expect(result[0]).toHaveProperty('height');
+      expect(result[0]).toHaveProperty('hashFunction');
+      expect(result[0]).toHaveProperty('hash');
+      expect(result[0]).toHaveProperty('url');
+    });
+  });
+
+  describe('#resizeBuffer', () => {
+    it('should resize buffer images', async () => {
+      const imageResponse = await axios.get('https://picsum.photos/200/300', {
+        responseType: 'arraybuffer',
+      });
+      const buffer = Buffer.from(imageResponse.data as string, 'binary');
+
+      const size = 10;
+      const result = await resizeBuffer(buffer as Buffer, 'image/jpeg', size);
+
+      expect(result.byteLength < buffer.byteLength);
+
+      const resizedDimensions = imageSize(result);
+
+      expect(resizedDimensions.height <= size);
+      expect(resizedDimensions.width <= size);
+    });
   });
 });
 
