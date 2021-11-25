ee2ebf05fe05187220fb5758afa49735d288f89a
Callum <callum@lukso.io>
2021-11-25 13:35:24 +0100

    docs: improve technical-reference docs generation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { LSPFactory } from './lib/lsp-factory';
 export { LSP3UniversalProfile } from './lib/classes/lsp3-universal-profile';
+export { DigitalAsset } from './lib/classes/digital-asset';
 export * from './lib/interfaces';
 export * from '../types/ethers-v5';
