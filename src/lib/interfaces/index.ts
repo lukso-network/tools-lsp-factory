3faadfe88e2cb73a682386574d2726077858f096
Callum <callum@lukso.io>
2022-03-02 11:58:59 +0100

    chore: improve Image type naming

--- src/lib/interfaces/index.ts
+++ src/lib/interfaces/index.ts
@@ -1,7 +1,12 @@
 export { ContractOptions } from './contract-options';
 export { LSPFactoryOptions } from './lsp-factory-options';
 export { LSP3Profile, LSP3ProfileJSON, ProfileDataBeforeUpload } from './lsp3-profile';
-export { ImageMetadata as Image, LinkMetdata as Link, ImageBuffer, SupportedImageBufferFormats } from './metadata';
+export {
+  ImageMetadata as Image,
+  LinkMetdata,
+  ImageBuffer,
+  SupportedImageBufferFormats,
+} from './metadata';
 
 export * from './profile-deployment';
 export * from './deployment-events';
