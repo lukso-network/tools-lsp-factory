ea33d1284d97e90f9bc18ae0931c85cd823678d2
Callum <callum@lukso.io>
2022-03-03 10:18:19 +0100

    chore: remove alias for ImageMetadata class

--- src/lib/interfaces/index.ts
+++ src/lib/interfaces/index.ts
@@ -1,12 +1,7 @@
 export { ContractOptions } from './contract-options';
 export { LSPFactoryOptions } from './lsp-factory-options';
 export { LSP3Profile, LSP3ProfileJSON, ProfileDataBeforeUpload } from './lsp3-profile';
-export {
-  ImageMetadata as Image,
-  LinkMetdata,
-  ImageBuffer,
-  SupportedImageBufferFormats,
-} from './metadata';
+export { ImageMetadata, LinkMetdata, ImageBuffer, SupportedImageBufferFormats } from './metadata';
 
 export * from './profile-deployment';
 export * from './deployment-events';
